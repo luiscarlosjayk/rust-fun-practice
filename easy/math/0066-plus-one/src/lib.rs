1506ff2cb4ef3f4f6765834af78cba1ff1fdb4f5
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2026-06-03 08:11:56 -0600

    chore: revisited the plun one and found a better solution

--- easy/math/0066-plus-one/src/lib.rs
+++ easy/math/0066-plus-one/src/lib.rs
@@ -1,35 +1,16 @@
 //! See README.md for the problem statement.
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Carry {
-    One,
-    Zero,
-}
-
 pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-    use Carry::*;
     let mut digits = digits;
-    let mut carry = Carry::One;
     for digit in digits.iter_mut().rev() {
-        match (carry, &mut *digit) {
-            (One, 9) => {
-                *digit = 0;
-            }
-            (One, n) => {
-                *n += 1;
-                carry = Zero;
-            }
-            (Zero, _) => {
-                // do nothing
-            }
+        if *digit == 9 {
+            *digit = 0;
+        } else {
+            *digit += 1;
+            return digits;
         }
     }
 
-    if carry == One {
-        digits.push(1);
-        let l = digits.len();
-        digits.swap(0, l-1);
-    }
-
+    digits.insert(0, 1); // Si llegamos aquí es porque todos eran 9, todos quedaron en 0
     digits
 }
