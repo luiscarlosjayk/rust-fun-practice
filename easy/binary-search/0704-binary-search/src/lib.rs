38368467bd350939690f618f0734cef886654031
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2026-06-07 12:56:45 -0600

    feat: solved easy/binary-search

--- easy/binary-search/0704-binary-search/src/lib.rs
+++ easy/binary-search/0704-binary-search/src/lib.rs
@@ -1,6 +1,18 @@
 //! See README.md for the problem statement.
 
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-    let _ = (&nums, target);
-    todo!("implement search")
+    let mut lo = 0;
+    let mut hi = nums.len();
+
+    while lo < hi {
+        let mid = lo + (hi - lo) / 2;
+        if nums[mid] == target {
+            return mid as i32;
+        } else if nums[mid] < target {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    -1
 }
