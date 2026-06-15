cdfa53a4b6a9c1b44456acc65f821a6a9c9f5070
Luis Carlos Osorio Jayk <luiscarlosjayk@gmail.com>
2026-06-14 20:21:22 -0600

    feat: solve search-insert-position with binary search

--- easy/binary-search/0035-search-insert-position/src/lib.rs
+++ easy/binary-search/0035-search-insert-position/src/lib.rs
@@ -1,6 +1,24 @@
 //! See README.md for the problem statement.
 
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let _ = (&nums, target);
-    todo!("implement search_insert")
+    if target < nums[0] {
+        return 0;
+    } else if target > nums[nums.len()-1] {
+        return nums.len() as i32;
+    }
+
+    let mut lo = 0;
+    let mut hi = nums.len();
+
+    while lo < hi {
+        let mid = lo + (hi - lo) / 2;
+        if nums[mid] == target {
+            return mid as i32;
+        } else if nums[mid] < target { 
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    lo as i32
 }
